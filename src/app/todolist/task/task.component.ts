ebbe7edc03bf5ce686a6cd6018fee201a04130eb
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-16 19:32:40 +0200

    Begin restyling task component

--- src/app/todolist/task/task.component.ts
+++ src/app/todolist/task/task.component.ts
@@ -7,4 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class TaskComponent {
   @Input() task: any;
+
+  isNoDescription() {
+    return this.task.description ? false : true;
+  }
+
+  isNoDueTime() {
+    return this.task.dueTime ? false : true;
+  }
 }

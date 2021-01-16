1c9fdadc90f5f54791148427c4d8967673dd3018
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-16 19:35:22 +0200

    Add new example task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   selectedList: any;
   todoLists: any[] = [
-      { name: "Important Tasks", tasks: [
-        { title: "Buy Food", description: "Bread, water, cat feed, etc.", done: false, dueTime: null },
-        { title: "Wash Car", done: true, dueTime: new Date("2022.01.01 00:00:00") }
-      ] },
-      { name: "Future Plans", tasks: [] },
-      { name: "Work", tasks: [] },
-      { name: "Ideas", tasks: [] },
-      { name: "Stuff", tasks: [] },
-    ];
+    {
+      name: "Important Tasks", tasks: [
+        { title: "Buy Goods", description: "Bread, water, cat feed, etc.", done: false, dueTime: null },
+        { title: "Wash Car", done: true, dueTime: new Date("2022.01.01 00:00:00") },
+        { title: "Write a Poem", description: "About programming patterns", done: false, dueTime: new Date("2022.01.01 00:00:00") }
+      ]
+    },
+    { name: "Future Plans", tasks: [] },
+    { name: "Work", tasks: [] },
+    { name: "Ideas", tasks: [] },
+    { name: "Stuff", tasks: [] },
+  ];
 
   selectTodoList(list: any) {
     this.selectedList = list;

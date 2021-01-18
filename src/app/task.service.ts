5eba99dea8e10698219abdc32b059b7ee32ad78a
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-18 09:41:56 +0200

    Add dashboard models, implement getDashboard at TaskService

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Dashboard } from './dashboard.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private endpoint: string = 'http://192.168.1.10:8080/';
+  private dashboardEndpoint: string = this.endpoint + "dashboard";
+  private tasksEndpoint: string = this.endpoint + "tasks";
+  private listsEndpoint: string = this.endpoint + "lists";
 
   constructor(private http: HttpClient) { }
+
+  getDashboard(): Observable<Dashboard> {
+    return this.http.get<Dashboard>(this.dashboardEndpoint);
+  }
 }
